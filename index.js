77c8a108dcbfbe59c93d60175aa8d2cfc3dec17f
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-25 22:14:34 -0400

    sum function, first test passing

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ export default class CleverArray {
   constructor(...args){
     this.array = args
   }
+  sum(){
+    return this.array.reduce( (accum, x) => accum + x )
+  }
 }
+
 // non babel implementation
 // module.exports = { CleverArray }
